37a559ca98e4059fb1077da9719df7dca4123495
XZZ999 <3036304796@qq.com>
2025-07-18 11:11:27 +0800

    增加提交了pycharm中的文件

--- test/git_test.py
+++ test/git_test.py
@@ -3,6 +3,5 @@ print('这个是对pycharm中的文件提交到github平台上的一次测试')
 print('这个是对pycharm中的文件提交到github平台上的一次测试')
 print('这个是对pycharm中的文件提交到github平台上的一次测试')
 print('这个是对pycharm中的文件提交到github平台上的一次测试')
-print('这个是对pycharm中的文件提交到github平台上的一次测试')
-print('这个是对pycharm中的文件提交到github平台上的一次测试')
+
 
